9154ed0685ac8f72b127a0c225a119575ea1d94d
Dantas <xp1998@live.com.pt>
2019-11-18 15:39:58 +0000

    messages

---
+++ src/main/java/Messages/Message.java
@@ -0,0 +1,40 @@
+package Messages;
+
+public abstract class Message {
+    private int id;
+    private String msg;
+
+    public Message(){
+        id = 0;
+        msg = "";
+    }
+
+    public Message(int id, String msg){
+        this.id = id;
+        this.msg = msg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{ " +
+                "id=" + id +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/Messages/VectorMessage.java
@@ -0,0 +1,75 @@
+package Messages;
+
+import java.util.Vector;
+
+public class VectorMessage extends Message {
+    Vector<Integer> v;
+
+    public VectorMessage(){
+        super();
+        this.v = new Vector<>();
+    }
+
+    public VectorMessage(int id, String msg){
+        super(id, msg);
+        this.v = new Vector<>();
+    }
+
+    public VectorMessage(int id, String msg, int numParticipants){
+        super(id, msg);
+        this.v = new Vector<>();
+        for(int i = 0; i<numParticipants; i++)
+            this.v.add(0);
+    }
+
+    public VectorMessage(VectorMessage m){
+        super(m.getId(), m.getMsg());
+        this.v = m.getVector();
+    }
+
+    public void fillFromByteArray (byte[] b){
+        String msg = new String(b);
+        String[] vars = msg.split("&");
+        this.setId(Integer.parseInt(vars[0]));
+        this.setMsg(vars[1]);
+        String[] counters = vars[2].split(",");
+        for(String c : counters)
+            v.add(Integer.parseInt(c));
+    }
+
+    public byte[] toByteArray(){
+        String res = "";
+        res += this.getId() + "&";
+        res += this.getMsg() + "&";
+        for(Integer i : v)
+            res+= Integer.toString(i) + ",";
+        return res.getBytes();
+    }
+
+    public void addToVector(int c){
+        this.v.add(c);
+    }
+
+    public void setVectorIndex(int index, int value){
+        this.v.set(index,value);
+    }
+
+    public Vector<Integer> getVector(){
+        return this.v;
+    }
+    public int getElement(int index){
+        return v.get(index);
+    }
+
+    @Override
+    public String toString() {
+        String vec="";
+        for(Integer i : v){
+            vec += Integer.toString(i) + '/';
+        }
+        return "VectorMessage{ " +
+                super.toString() +
+                " v= " + vec +
+                '}';
+    }
+}
